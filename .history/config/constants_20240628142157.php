0b851b009b6b8e2757125561a3518bcb9909d117
Emmanuel Agyemang <emmanuelagyemang12345@gmail.com>
2024-06-28 14:24:27 +0000

    adding constant and .history to repository

---
+++ .history/config/constants_20240628142157.php
@@ -0,0 +1,9 @@
+<?php
+
+session_start();
+
+define('ROOT_URL', 'http://localhost/blogwebapp/');
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '');
+    define('DB_PASSWORD', 'admin1234');
+    define('DB_NAME', 'hostel');

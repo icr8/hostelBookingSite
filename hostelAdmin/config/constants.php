f0438c7da1bf3bac7092063e379e3743a3cb7a67
Emmanuel Agyemang <emmanuelagyemang12345@gmail.com>
2024-07-14 19:33:17 +0000

    update to constant

--- hostelAdmin/config/constants.php
+++ hostelAdmin/config/constants.php
@@ -4,6 +4,6 @@ session_start();
 
 define('ROOT_URL', 'http://localhost/blogwebapp/');
     define('DB_HOST', 'localhost');
-    define('DB_USER', 'emma');
+    define('DB_USER', '');
     define('DB_PASSWORD', '');
     define('DB_NAME', 'hostel');
